aaf4dc68b0206ade9b98e37b101f600b80c665a7
lucaslbatista <lucaslbatista@gmail.com>
2021-01-19 13:34:25 -0300

    feat: :sparkles: criaçao da estrutura de componentes das indicaçoes

---
+++ src/app/modules/indicacao/indicacao-routing.module.ts
@@ -0,0 +1,17 @@
+import { CadastroComponent } from './../indicacao/containers/cadastro/cadastro.component';
+import { ConsultaComponent } from './../indicacao/containers/consulta/consulta.component';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Routes } from '@angular/router';
+
+
+export const INDICACAO_ROUTES: Routes = [
+  { path: '', component: ConsultaComponent, data: { breadcrumb: null } },
+  { path: 'criar', component: CadastroComponent, data: { breadcrumb: null } }
+];
+
+@NgModule({
+  declarations: [],
+  imports: [CommonModule],
+})
+export class CategoriasRoutingModule {}
