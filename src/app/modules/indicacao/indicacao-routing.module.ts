7e505c0b858e83c717bc5bc2f02acc3b2490b1de
andreibarroso <andrei.mesquita@globomail.com>
2021-01-22 08:44:50 -0300

    feat (indicacao) :sparkles: Cria components edicao e detalhe

--- src/app/modules/indicacao/indicacao-routing.module.ts
+++ src/app/modules/indicacao/indicacao-routing.module.ts
@@ -1,17 +1,18 @@
-import { CadastroComponent } from './../indicacao/containers/cadastro/cadastro.component';
-import { ConsultaComponent } from './../indicacao/containers/consulta/consulta.component';
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { Routes } from '@angular/router';
+import {CadastroComponent} from './../indicacao/containers/cadastro/cadastro.component';
+import {ConsultaComponent} from './../indicacao/containers/consulta/consulta.component';
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {Routes} from '@angular/router';
 
 
 export const INDICACAO_ROUTES: Routes = [
-  { path: '', component: ConsultaComponent, data: { breadcrumb: null } },
-  { path: 'criar', component: CadastroComponent, data: { breadcrumb: null } }
+  {path: '', component: ConsultaComponent, data: {breadcrumb: null}},
+  {path: 'criar', component: CadastroComponent, data: {breadcrumb: null}}
 ];
 
 @NgModule({
   declarations: [],
   imports: [CommonModule],
 })
-export class CategoriasRoutingModule {}
+export class IndicacaoRoutingModule {
+}
--- src/app/modules/indicacao/indicacao.module.ts
+++ src/app/modules/indicacao/indicacao.module.ts
@@ -5,11 +5,13 @@ import { CadastroComponent } from './containers/cadastro/cadastro.component';
 import { ConsultaComponent } from './containers/consulta/consulta.component';
 import { RouterModule } from '@angular/router';
 import { PrimengComponentsModule } from 'src/app/shared/primeng.components.module';
+import { DetalheComponent } from './containers/detalhe/detalhe.component';
+import { EdicaoComponent } from './containers/edicao/edicao.component';
 
 
 
 @NgModule({
-  declarations: [CadastroComponent, ConsultaComponent],
+  declarations: [CadastroComponent, ConsultaComponent, DetalheComponent, EdicaoComponent],
   imports: [
     CommonModule,
     RouterModule.forChild(INDICACAO_ROUTES),
