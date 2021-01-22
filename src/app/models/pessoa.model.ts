2bf033e4e007408e9a44503c70b14ed7c81b542a
andreibarroso <andrei.mesquita@globomail.com>
2021-01-22 15:05:34 -0300

    feat (indicacao) :sparkles: consulta indicacao

--- src/app/models/pessoa.model.ts
+++ src/app/models/pessoa.model.ts
@@ -3,6 +3,8 @@ import { BaseModel } from './../core/models/base.model';
 import {BaseSearch} from '../core/models/base-search.model';
 import { Indicacao, IndicacaoResponse } from './indicacao.model';
 
+
+
 export interface Pessoa extends BaseModel {
   nome: string;
   nrCpf: string;
@@ -13,8 +15,17 @@ export interface Pessoa extends BaseModel {
 }
 
 export interface PessoaSearch extends BaseSearch {
+  nrCpf?: string;
   nome?: string;
+  siglaPosto?: string;
+  siglaQuadro?: string;
+  siglaEspecialidade?: string;
+  situacao?: string;
+  email?: string;
+  organizacaoMilitarId?: number;
+  contatoPrincipal?: string;
 }
+
 
 export interface PessoaIndicada{
   prioridade?: number;
