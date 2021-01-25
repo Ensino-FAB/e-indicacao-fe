7a2e56fd3fa419897dc9c4c1db9235f0f232dc37
andreibarroso <andrei.mesquita@globomail.com>
2021-01-25 09:09:40 -0300

    feat (indicação) :sparkles: Cria components inciais para implementacao

---
+++ src/app/modules/indicacao/components/autocomplete-pessoas/autocomplete-pessoas.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-autocomplete-pessoas',
+  templateUrl: './autocomplete-pessoas.component.html',
+  styleUrls: ['./autocomplete-pessoas.component.scss']
+})
+export class AutocompletePessoasComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/modules/indicacao/components/cadastro-step1/cadastro-step1.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cadastro-step1',
+  templateUrl: './cadastro-step1.component.html',
+  styleUrls: ['./cadastro-step1.component.scss']
+})
+export class CadastroStep1Component implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
