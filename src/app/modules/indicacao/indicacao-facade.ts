e00ff68f108414a8f01cba7c9e505476ec055c91
andreibarroso <andrei.mesquita@globomail.com>
2021-01-25 13:57:07 -0300

    feat (indicacao) :sparkles: Implementa step1

--- src/app/modules/indicacao/indicacao-facade.ts
+++ src/app/modules/indicacao/indicacao-facade.ts
@@ -3,10 +3,12 @@ import {Injectable, Injector} from '@angular/core';
 import {IndicacaoService} from '../../services/indicacao.service';
 import {PessoaService} from '../../services/pessoa.service';
 import {EventoService} from "../../services/evento.service";
+import {OrganizacaoService} from "../../services/organizacao.service";
 
 
 @Injectable()
 export class IndicacaoFacade {
+
   // tslint:disable-next-line:variable-name
   private _indicacaoService: IndicacaoService;
 
@@ -15,6 +17,9 @@ export class IndicacaoFacade {
 
   // tslint:disable-next-line:variable-name
   private _eventoService: EventoService;
+
+  // tslint:disable-next-line:variable-name
+  private _organizacaoService: OrganizacaoService;
 
 
   constructor(private injector: Injector) {
@@ -40,6 +45,13 @@ export class IndicacaoFacade {
       this._eventoService = this.injector.get(EventoService);
     }
     return this._eventoService;
+  }
+
+  public get organizacaoService(): OrganizacaoService {
+    if (!this._organizacaoService) {
+      this._organizacaoService = this.injector.get(OrganizacaoService);
+    }
+    return this._organizacaoService;
   }
 
 }
