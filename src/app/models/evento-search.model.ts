a15f156c95fa65b9ac3ec47b2ec227cb2fb0da8d
andreibarroso <andrei.mesquita@globomail.com>
2021-01-26 09:12:42 -0300

    feat (indicação) :sparkles: Implementa buscas

--- src/app/models/evento-search.model.ts
+++ src/app/models/evento-search.model.ts
@@ -1,7 +1,9 @@
 import {BaseSearch} from '../core/models/base-search.model';
+import {Evento} from "./evento.model";
 
 
 export interface EventoSearchModel extends BaseSearch {
   nome: string;
   id: number;
+  evento: Evento;
 }
