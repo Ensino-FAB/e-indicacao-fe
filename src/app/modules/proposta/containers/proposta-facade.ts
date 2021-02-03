197dc5d702680a6f3ca153c26de02f995860fb3c
andreibarroso <andrei.mesquita@globomail.com>
2021-02-03 09:10:32 -0300

    feat (proposta) :sparkles: Implementa comunicação com serviço de encerrar proposta

--- src/app/modules/proposta/containers/proposta-facade.ts
+++ src/app/modules/proposta/containers/proposta-facade.ts
@@ -1,43 +1,47 @@
-import { OrganizacaoService } from './../../../services/organizacao.service';
-import { EventoService } from './../../../services/evento.service';
-import { PropostaResponse } from './../../../models/proposta.model';
-import { Pageable } from './../../../core/models/pageable.model';
-import { PropostaSearchModel } from './../../../models/proposta-search.model';
-import { PropostaService } from './../../../services/proposta.service';
+import {OrganizacaoService} from './../../../services/organizacao.service';
+import {EventoService} from './../../../services/evento.service';
+import {PropostaResponse} from './../../../models/proposta.model';
+import {Pageable} from './../../../core/models/pageable.model';
+import {PropostaSearchModel} from './../../../models/proposta-search.model';
+import {PropostaService} from './../../../services/proposta.service';
 import {Indicacao, IndicacaoResponse} from './../../../models/indicacao.model';
-import { IndicacaoService } from './../../../services/indicacao.service';
-import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
-import { PropostaRequest } from 'src/app/models/proposta.model';
+import {IndicacaoService} from './../../../services/indicacao.service';
+import {Injectable} from '@angular/core';
+import {Observable} from 'rxjs';
+import {PropostaRequest} from 'src/app/models/proposta.model';
 import {IndicacaoSearchModel} from '../../../models/indicacao-search.model';
-import { Organizacao } from 'src/app/models/organizacao.model';
+import {Organizacao} from 'src/app/models/organizacao.model';
 
 @Injectable()
 export class PropostaFacade {
-    constructor(
-      private indicacaoService: IndicacaoService,
-      private propostaService: PropostaService,
-      private organizacaoService: OrganizacaoService
-      ) {
-    }
+  constructor(
+    private indicacaoService: IndicacaoService,
+    private propostaService: PropostaService,
+    private organizacaoService: OrganizacaoService
+  ) {
+  }
 
-    findAllIndicacoesByEvento(search: IndicacaoSearchModel, idEvento: number): Observable<IndicacaoResponse[]> {
-        return this.indicacaoService.findAllIndicacoesByEvento(search, idEvento);
-    }
+  findAllIndicacoesByEvento(search: IndicacaoSearchModel, idEvento: number): Observable<IndicacaoResponse[]> {
+    return this.indicacaoService.findAllIndicacoesByEvento(search, idEvento);
+  }
 
-    findAllPropostas(search: PropostaSearchModel): Observable<Pageable<PropostaResponse>> {
-      return this.propostaService.findAll(search);
-    }
+  findAllPropostas(search: PropostaSearchModel): Observable<Pageable<PropostaResponse>> {
+    return this.propostaService.findAll(search);
+  }
 
-    createProposta(record: PropostaRequest): Observable<PropostaRequest>{
-      return this.propostaService.create(record);
-    }
+  createProposta(record: PropostaRequest): Observable<PropostaRequest> {
+    return this.propostaService.create(record);
+  }
 
-    deleteProposta(id: number): Observable<any>{
-      return this.propostaService.delete(id);
-    }
+  deleteProposta(id: number): Observable<any> {
+    return this.propostaService.delete(id);
+  }
 
-    findOrganizacoesDiretamenteSubordinadas(cdOrg: string): Observable<Organizacao[]>{
-      return this.organizacaoService.findOrganizacoesDiretamenteSubordinadas(cdOrg);
-    }
+  findOrganizacoesDiretamenteSubordinadas(cdOrg: string): Observable<Organizacao[]> {
+    return this.organizacaoService.findOrganizacoesDiretamenteSubordinadas(cdOrg);
+  }
+
+  finishProposta(id: number): Observable<any> {
+    return this.propostaService.encerrarProposta(id);
+  }
 }
