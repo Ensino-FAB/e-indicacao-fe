6e59dc90a18867f657324c460504c691c7436901
andreibarroso <andrei.mesquita@globomail.com>
2021-02-09 08:52:32 -0300

    feat (indicacao) :sparkles: Cria environment de homolagação

---
+++ src/environments/environment.homolog.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  production: true,
+  CURSOS_INDICACAO_API: 'https://api.servicos.homolog.ccarj.intraer/esistens-indicacao-api/',
+  KEYCLOAK_URL: 'https://auth.homolog.ccarj.intraer/auth',
+  KEYCLOAK_REALM: 'FAB',
+  KEYCLOAK_CLIENT_ID: 'ensino-client',
+  KEYCLOAK_REDIRECT_URI:
+    'https://sigpes.servicos.homolog.ccarj.intraer/esistens-organizacional/assets/silent-check-sso.html',
+};
+
